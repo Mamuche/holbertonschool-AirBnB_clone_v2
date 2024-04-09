59df365d56b718715a301e20dd3d3a48b70c91bc
Mamuche <marionlaroche17@gmail.com>
2024-04-09 15:34:06 +0200

    task5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Write a script that starts a Flask web application:
+Routes:
+/number_template/<n>: display a HTML page only if n is an integer:
+H1 tag: “Number: n” inside the tag BODY """
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    return "C " + text.replace("_", " ")
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    return "Python " + text.replace("_", " ")
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    if type(n) is int:
+        return "{} is a number".format(n)
+    
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template("5-number.html", n=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
